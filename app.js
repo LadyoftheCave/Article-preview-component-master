94894b925752540fda7450f13c21d21f664d2110
LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
2021-04-10 15:57:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function someFunction() {
+    document.getElementById("buttontest").style.visibility = "visible";
+}
+
+function someFunctionTwo() {
+    document.getElementById("buttonmedia").style.visibility = "visible";
+    document.getElementById("michelle").style.display = "none";
+}
